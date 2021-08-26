c6451cc42c9e5e6534cdcfd425bde0d9491a65b9
diego <dgvlc@yahoo.com>
2021-08-26 15:35:04 -0400

    small addition to backend

---
+++ db/schema.rb
@@ -0,0 +1,59 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_08_26_185348) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "cars", force: :cascade do |t|
+    t.string "name"
+    t.string "make"
+    t.string "model"
+    t.integer "year"
+    t.text "description"
+    t.string "color"
+    t.text "image_url"
+    t.integer "base_price"
+    t.integer "upgrade_price"
+    t.integer "total_price"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_cars_on_user_id"
+  end
+
+  create_table "cars_upgrades", id: false, force: :cascade do |t|
+    t.bigint "car_id", null: false
+    t.bigint "upgrade_id", null: false
+  end
+
+  create_table "upgrades", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.integer "price"
+    t.text "image_url"
+    t.string "type"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "cars", "users"
+end
