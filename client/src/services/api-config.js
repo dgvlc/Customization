5805b3ad59983bdd1e2b34078594fa4c0ec5c2c7
diego <dgvlc@yahoo.com>
2021-08-27 15:04:57 -0400

    added api-config

---
+++ client/src/services/api-config.js
@@ -0,0 +1,11 @@
+import axios from 'axios'
+
+const baseURL = process.env.NODE_ENV === 'production'
+  ? 'https://customization-api.herokuapp.com/'
+  : 'http://localhost:3000'
+
+const api = axios.create({
+  baseURL: baseURL
+}); 
+
+export default api 
